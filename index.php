1235e39630084f03fedb1912ea4ee78d0108f101
frimson <46135627+frimson@users.noreply.github.com>
2020-02-02 23:29:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>retard</title>
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="super-block">
+    <div class="head">
+    <div class="head__content">
+        <div class="head__result"> <img src="img/1.jpg"> </div>
+        <div class="head__text">
+        <h1>The Dacenko Email Subject Line Tester</h1>
+            <a>Boost your email open rates: analyze and improve your subject lines with our free tester before sending out your next campaign.</a>
+        </div>
+        <div class="head-input">
+            <input type="text" class="head-input__text" placeholder="За">
+            <a href="#" class="head-input__btn">лупа</a>
+        </div>
+    </div>
+    </div>
+
+
+    <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+
+
+    <div class="content">
+    <div class="block2">
+        <div class="block2-pic">
+            <img src="img/1.jpg">
+        </div>
+        <div class="block2-text">
+            <h1 id="block2-text__h1">Hello, Bear! Are you okay?<br> Maybe some beer?</h1>
+            <a>Nice! A few small tweaks and you're good to go. See below for
+                suggestions on how to make your subject line more impactful.
+            </a>
+        </div>
+    </div>
+        <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+    </div>
+
+
+    <p id="Length">Length</p>
+    <div class="block3">
+
+        <div class="block3-leftbox">
+            <p style="font-size: 30pt">42</p>
+            <p style="margin-bottom: 1%">Characters</p>
+            <img  width="90%" height="40px" src="img/hor.jpg">
+            <p style="font-weight: bold">Character count</p><br>
+            <p>Your subject line is a bit long. Subject lines
+                with 21-40 characters tend to see higher
+                open rates. Can you make yours any
+                shorter?</p>
+
+        </div>
+
+        <div class="block3-rightbox">
+            <p style="font-size: 30pt">8</p>
+            <p style="margin-bottom: 1%">Words</p>
+            <img  width="90%" height="40px" src="img/hor.jpg">
+            <p style="font-weight: bold">Word count</p><br>
+            <p>Your subject line contains helpful words,
+                nice! People ate more likely to open emails
+                when they see words like these.
+            </p>
+        </div>
+    </div>
+
+
+    <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+
+    <p id="Wording">Wording</p>
+    <div class="block4">
+        <div class="block4-leftbox">
+
+            <img  width="50px" height="50px" src="img/face.png" style="margin-bottom: 2%">
+            <p id="boldtext">Helpful words</p><br>
+            <p>Your subject line contains helpful words,
+                nice! People ate more likely to open emails
+                when they see words like these.</p>
+
+        </div>
+
+        <div class="block4-rightbox">
+            <img  width="50px" height="50px" src="img/face.png" style="margin-bottom: 2%">
+            <p id="boldtext">Negative words</p><br>
+            <p>Phew! Your subject line doesn’t contain any
+                words that could be considered harmful.
+            </p>
+        </div>
+    </div>
+
+
+    <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+
+    <div class="block5">
+        <p id="SpamFolderAlerts">Spam Folder Alerts</p>
+        <div class="block5-rightbox">
+            <div class="block5-1ch">
+                <img width="50px" height="50px" src="img/face.png" style="margin: 0 5% 0 0">
+                    <div class="block5-1ch__text">
+                    <p id="boldtext">Spammy Characters</p>
+                    <p >Good job! Spammy characters are nowhere to
+                    be found.</p>
+                    </div>
+            </div>
+            <div class="block5-2ch">
+                <img width="50px" height="50px" src="img/face.png" style="margin: 0 5% 0 0">
+                    <div class="block5-2ch__text">
+                    <p id="boldtext">Spammy Words</p>
+                    <p >The words <a style="background-color: rgba(255,54,189,0.76)">Hello</a>. might land your email in the
+                    Spam folder. We recommend you look for an
+                    alternative.</p>
+                    </div>
+            </div>
+            <div class="block5-3ch">
+                <img width="50px" height="50px" src="img/face.png" style="margin: 0 5% 0 0">
+                    <div class="block5-3ch__text">
+                    <p id="boldtext">Excessive Punctuation</p>
+                    <p >No excessive punctuation here! Nice work..</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+
+    <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+
+
+    <div class="block6">
+        <p id="Scannability">Scannability</p>
+        <div class="block6-top">
+            <p id="block6-top__text1">Capitalization Style</p>
+            <p id="block6-top__text2"><span style="background-color: #51faff">Sentence Case</span></p>
+            <p id="block6-top__text3">Bravo! Subject lines perform best when they're
+                written in sentence case — like yours.
+            </p>
+        </div>
+        <hr align="center" width="100%" size="1" color="grey" style="margin-bottom: 50px" />
+        <div class="block6-bottom">
+            <p id="block6-bottom__text1">Use of Numbers</p>
+            <p id="block6-bottom__text2"><span style="background-color: #af66ff">None</span></p>
+            <p id="block6-bottom__text3">Numbers help subject lines stand out and are
+                proven to increase open rates. Using one in
+                yours could make all the difference! If it fits the
+                context, of course.
+            </p>
+        </div>
+    </div>
+
+    <hr align="center" width="100%" size="2" color="black" style="margin-bottom: 50px" />
+    <a href="#" class="end-btn">Test Again</a>
+</div>
+</body>
+</html>
